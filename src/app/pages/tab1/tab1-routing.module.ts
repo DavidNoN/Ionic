bc3e15bbef7233df99d946f0295e110abe7cf543
DavidNoN <davidnon@utp.edu.co>
2020-06-13 15:54:55 -0500

    initial commit

--- src/app/pages/tab1/tab1-routing.module.ts
+++ src/app/pages/tab1/tab1-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Tab1Page } from './tab1.page';
+import { AgregarPage } from '../agregar/agregar.page';
+
 
 const routes: Routes = [
   {
     path: '',
     component: Tab1Page,
+  },
+  {
+    path: 'agregar/:listaId',
+    loadChildren: () => import('../agregar/agregar.module').then( m => m.AgregarPageModule)
   }
 ];
 
